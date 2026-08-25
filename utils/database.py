c42b5f028ecd454b105ed544f9fa81702484de6f
Aryan Pukale <aryanpukale23@gmail.com>
2026-08-25 18:17:45 +0530

    Refactor database connection and add init_db function
    Added database initialization and schema loading functionality.

--- utils/database.py
+++ utils/database.py
@@ -1,9 +1,63 @@
 import sqlite3
+import os
 
-DATABASE = "database/database.db"
+# DATABASE PATH
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASE = os.path.join(BASE_DIR, "database", "database.db")
+
+# SCHEMA PATH
+SCHEMA_FILE = os.path.join(
+    BASE_DIR,
+    "database",
+    "schema.sql"
+)
 
 
 def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
+
+    # Make sure database folder exists
+    os.makedirs(
+        os.path.dirname(DATABASE),
+        exist_ok=True
+    )
+
+    conn = sqlite3.connect(
+        DATABASE
+    )
+
     conn.row_factory = sqlite3.Row
+
     return conn
+
+
+def init_db():
+
+    # Make sure database folder exists
+    os.makedirs(
+        os.path.dirname(DATABASE),
+        exist_ok=True
+    )
+
+    conn = sqlite3.connect(
+        DATABASE
+    )
+
+    try:
+
+        with open(
+            SCHEMA_FILE,
+            "r",
+            encoding="utf-8"
+        ) as file:
+
+            schema = file.read()
+
+        conn.executescript(
+            schema
+        )
+
+        conn.commit()
+
+    finally:
+
+        conn.close()
